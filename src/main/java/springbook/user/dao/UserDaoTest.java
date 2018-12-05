4206994d6f9bc9335bf10b78a0c066b62f74f1a4
KIMINJOONG <ijboym216@gmail.com>
2018-12-05 21:22:58 +0900

    useing bean and applicaitionContext

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,12 +2,15 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao dao = new UserDao(connectionMaker);
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship");
