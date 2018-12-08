e09932bb46f3e6277ce240a65f1d8a5f3fe2b831
KIMINJOONG <ijboym216@gmail.com>
2018-12-08 20:09:23 +0900

    create to TestClass with jUnit

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,32 +1,33 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
+import org.junit.Test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
 		
 		dao.add(user);
 		
-		System.out.println(user.getId() + "등록성공");
-		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		
-		System.out.println(user2.getPassword());
-		System.out.println(user2.getId() + "조회 성공");
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 	}
 }
