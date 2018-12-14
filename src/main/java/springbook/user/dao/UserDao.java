541fdcd5434483d56a270bacfff4bfef7153672f
KIMINJOONG <ijboym216@gmail.com>
2018-12-14 23:39:25 +0900

    anonymous inner class

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -17,18 +17,18 @@ public class UserDao {
 	}
 	
 	
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement(
-				"insert into users(id, name, password) values(?, ?, ?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
-		
+	public void add(final User user) throws ClassNotFoundException, SQLException {
+		jdbcContextWithStatementStrategy( new StatementStrategy() {
+			@Override
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password)"
+				+ "values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				return ps;
+			}
+		});
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
@@ -54,8 +54,13 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			@Override
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("delete from users");
+				return ps;
+			}
+		});
 	}
 	
 	private PreparedStatement makeStatement(Connection c) throws SQLException {
