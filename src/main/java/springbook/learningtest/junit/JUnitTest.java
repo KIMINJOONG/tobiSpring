c9406967c47a7adc3cf8417140d5b38b07bd9afb
KIMINJOONG <ijboym216@gmail.com>
2018-12-17 23:14:50 +0900

    update UserDao.java

--- src/main/java/springbook/learningtest/junit/JUnitTest.java
+++ src/main/java/springbook/learningtest/junit/JUnitTest.java
@@ -5,7 +5,6 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -16,7 +15,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -5,30 +5,39 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 public class UserDao {
-	private javax.sql.DataSource dataSource;
+	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 	
-	public void setDataSource(javax.sql.DataSource dataSource) {
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+
+
+	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 	
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
-		jdbcContextWithStatementStrategy( new StatementStrategy() {
-			@Override
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password)"
-				+ "values(?,?,?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				return ps;
-			}
-		});
+		this.jdbcContext.workWithStatementStrategy(
+				new StatementStrategy() {
+					@Override
+					public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+						PreparedStatement ps = c.prepareStatement("insert into users(id, name, password)"
+								+ "values(?,?,?)");
+								ps.setString(1, user.getId());
+								ps.setString(2, user.getName());
+								ps.setString(3, user.getPassword());
+								return ps;
+					}
+				}
+				);
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
@@ -54,7 +63,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			@Override
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 				PreparedStatement ps = c.prepareStatement("delete from users");
@@ -68,7 +77,7 @@ public class UserDao {
 		ps = c.prepareStatement("delete from users");
 		return ps;
 	}
-	
+	/*
 	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
 		Connection c = null;
 		PreparedStatement ps = null;
@@ -84,7 +93,7 @@ public class UserDao {
 			if(c != null) {try{c.close();} catch(SQLException e) {} }
 		}
 	}
-	
+	*/
 	public int getCount() throws SQLException {
 		Connection c = null;
 		PreparedStatement ps = null;
