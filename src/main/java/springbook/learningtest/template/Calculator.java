8eec19dd731c641f2c5ddbd250bd780efb435e7d
KIMINJOONG <ijboym216@gmail.com>
2018-12-26 23:18:53 +0900

    LineCallback 인터페이스 <T>추가

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException {
-		LineCallback sumCallback =
-				new LineCallback() {
+		LineCallback<Integer> sumCallback =
+				new LineCallback<Integer>() {
 					
 					@Override
 					public Integer doSomethingWithLine(String line, Integer value) {
@@ -18,8 +18,8 @@ public class Calculator {
 	}
 	
 	public Integer calcMultiply(String filepath) throws IOException {
-		LineCallback multiplyCallback = 
-				new LineCallback() {
+		LineCallback<Integer> multiplyCallback = 
+				new LineCallback<Integer>() {
 					
 					@Override
 					public Integer doSomethingWithLine(String line, Integer value) {
@@ -48,11 +48,11 @@ public class Calculator {
 		}
 	}
 	
-	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer res = initVal;
+			T res = initVal;
 			String line = null;
 			while((line = br.readLine()) != null) {
 				res = callback.doSomethingWithLine(line, res);
@@ -68,5 +68,17 @@ public class Calculator {
 			}
 		}
 		return initVal;
+	}
+	
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback = 
+				new LineCallback<String>() {
+					
+					@Override
+					public String doSomethingWithLine(String line, String value) {
+						return value + line;
+					}
+				};
+				return lineReadTemplate(filepath, concatenateCallback, "");
 	}
 }
