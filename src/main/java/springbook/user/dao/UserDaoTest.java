baad646adfece3ced5377e1995df6b39b66a3060
KIMINJOONG <ijboym216@gmail.com>
2019-01-02 23:46:57 +0900

    UserDaoTest에 중복테스트 함수 추가 UserDao인터페이스 추가 기존의 UserDao -> UserDaoJdbc로 변경

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,18 +25,16 @@ import springbook.user.domain.User;
 public class UserDaoTest {
 	
 //	private ApplicationContext context;
-	UserDao dao;
+	@Autowired
+	private UserDao dao;
 	private User user1;
 	private User user2;
 	private User user3;
 	
 	@Before
 	public void setUp() {
-//		this.dao = this.context.getBean("userDao", UserDao.class);
-		dao = new UserDao();
 		DataSource dataSource = new SingleConnectionDataSource(
 				"jdbc:mysql://localhost/testdb?serverTimezone=UTC", "spring", "book", true);
-		dao.setDataSource(dataSource);
 		this.user1 = new User("gyumee", "박성철", "springno1");
 		this.user2 = new User("leegw700", "이길원", "springno2");
 		this.user3 = new User("bumjin", "박범진", "springno3");
@@ -118,4 +118,14 @@ public class UserDaoTest {
 		assertThat(user1.getPassword(), is(user2.getPassword()));
 		
 	}
+	
+	@Test(expected=DataAccessException.class) 
+	public void duplciateKey() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		dao.add(user1);
+	}
+	
+	
 }
