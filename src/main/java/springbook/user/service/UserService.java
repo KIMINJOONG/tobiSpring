56c841c3035710dade21fe0df7dda710e9e849ba
KIMINJOONG <ijboym216@gmail.com>
2019-01-07 21:42:26 +0900

    userService랑 userServiceTest수정

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -2,9 +2,13 @@ package springbook.user.service;
 
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 public class UserService {
 	UserDao userDao;
@@ -13,6 +17,20 @@ public class UserService {
 		this.userDao = userDao;
 	}
 	
-	
+	@Test
+	public void upgradeLevels() {
+		List<User> users = userDao.getAll();
+		for(User user : users) {
+			Boolean changed = null;
+			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+				user.setLevel(Level.SILVER);
+				changed = true;
+			} else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+				changed = true;
+			} else if(user.getLevel() == Level.GOLD) { changed = false; }
+			else { changed = false; }
+			if(changed) { userDao.update(user); }
+		}
+	}
 	
 }
--- src/main/java/springbook/user/service/UserServiceTest.java
+++ src/main/java/springbook/user/service/UserServiceTest.java
@@ -4,11 +4,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/test-applicationContext.xml")
@@ -16,8 +23,16 @@ public class UserServiceTest {
 	@Autowired
 	UserService userService;
 	
-	@Test
-	public void bean() {
-		assertThat(this.userService, is(notNullValue()));
+	List<User> users;
+	@Before
+	public void setUp() {
+		users = Arrays.asList(//배열을 리스트로 만들어주는 메소드 배열을 가변인자로 넣어주면 더욱 편리
+				new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0)
+				, new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0)
+				, new User("erwins", "신승한", "p3", Level.SILVER, 60, 29)
+				, new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30)
+				, new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+		);
 	}
+	
 }
