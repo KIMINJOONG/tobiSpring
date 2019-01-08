b9f75825b93e62e24428e58715ee5caa623d8ede
KIMINJOONG <ijboym216@gmail.com>
2019-01-08 21:46:56 +0900

    upgrade메소드 리팩토링, enum Level 수정

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -66,5 +66,14 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public void upgradeLevel() {
+		Level nextLevel = this.level.nextLevel();
+		if(nextLevel == null) {
+			throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+		} else {
+			this.level = nextLevel;
+		}
+	}
+	
 	
 }
