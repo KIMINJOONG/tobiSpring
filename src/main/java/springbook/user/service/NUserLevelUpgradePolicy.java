fc827cd28e3636f672e57d870f036d10fc2735a6
KIMINJOONG <ijboym216@gmail.com>
2019-01-10 21:49:01 +0900

    UserUpgradePolicy적용

---
+++ src/main/java/springbook/user/service/NUserLevelUpgradePolicy.java
@@ -0,0 +1,31 @@
+package springbook.user.service;
+
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static springbook.user.service.UserService.MIN_RECCOMEND_FOR_GOLD;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import springbook.user.dao.UserDao;
+
+public class NUserLevelUpgradePolicy implements UserLevelUpgradePolicy{
+
+	@Override
+	public boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch (currentLevel) {
+		case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+		case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+		case GOLD : return false;
+		default : throw new IllegalArgumentException("Unknown Level : " + currentLevel);
+		}
+	}
+
+	@Override
+	public void upgradeLevel(User user, UserDao userDao) {
+		user.upgradeLevel();
+		userDao.update(user);
+	}
+
+}
